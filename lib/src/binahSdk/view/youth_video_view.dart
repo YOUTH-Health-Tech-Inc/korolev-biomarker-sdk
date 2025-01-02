c7319aca55762c3307e489d6d8d426a1d1d1b455
oleksandr.kyslyi <oleksandr.kyslyi@youth-prevention.com>
2025-01-02 07:35:01 +0100

    fix: fix code

--- lib/src/binahSdk/view/youth_video_view.dart
+++ lib/src/binahSdk/view/youth_video_view.dart
@@ -1,5 +1,11 @@
 import 'package:biosensesignal_flutter_sdk/ui/camera_preview_view.dart';
+import 'package:flutter/cupertino.dart';
 
-class YouthVideoView extends CameraPreviewView {
+class YouthVideoView extends StatelessWidget {
   const YouthVideoView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CameraPreviewView();
+  }
 }
